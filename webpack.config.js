be5d7a19905686c52c5134e80579d10a49688c32
Rafael Anachoreta <rafael.anachoreta@gmail.com>
2023-06-10 16:53:21 +0100

    feat: add tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
       ],
     },
